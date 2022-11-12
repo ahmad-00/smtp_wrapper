b6ef313abc3935f54f2c706dc7ce02c355681762
ahmad-00 <mohammadi.ahmad00@gmail.com>
2022-11-12 17:28:38 +0300

    base_email minor bug fixed

--- base_email.py
+++ base_email.py
@@ -1,5 +1,6 @@
 import os
 import re
+from typing import Optional
 
 from config import Config
 from smtp_client import SMTPClient
@@ -8,7 +9,7 @@ from smtp_client import SMTPClient
 class BaseEmail:
     _smtp = None
     subject: str
-    sender: str
+    sender: Optional[str] = None
     recipient_email: str
     html_body: str
     _email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
